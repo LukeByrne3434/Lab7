c1d32fca8fb3630aaad8bef32e14be3e00dd0476
Max Power <lukebyrne3434@gmail.com>
2020-03-09 17:41:27 +0000

    first commit

---
+++ myApp/src/app/app-routing.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  
+  {
+    path: 'ulster',
+    loadChildren: () => import('./ulster/ulster.module').then( m => m.UlsterPageModule)
+  },
+  {
+    path: 'munster',
+    loadChildren: () => import('./munster/munster.module').then( m => m.MunsterPageModule)
+  },
+  {
+    path: 'connaught',
+    loadChildren: () => import('./connaught/connaught.module').then( m => m.ConnaughtPageModule)
+  },
+  {
+    path: 'leinster',
+    loadChildren: () => import('./leinster/leinster.module').then( m => m.LeinsterPageModule)
+  },
+  {
+    path: 'leinstercounties',
+    loadChildren: () => import('./leinstercounties/leinstercounties.module').then( m => m.LeinstercountiesPageModule)
+  },
+  {
+    path: 'connaughtcounties',
+    loadChildren: () => import('./connaughtcounties/connaughtcounties.module').then( m => m.ConnaughtcountiesPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
